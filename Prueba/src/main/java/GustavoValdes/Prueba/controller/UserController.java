6d7fe2a93615fa0240a95efecc57fbafad019862
GustavoValdesZ <gustavovaldes.z@gmail.com>
2026-05-15 20:00:09 -0400

    crear UserController con endpoints POST y GET para usuarios

---
+++ Prueba/src/main/java/GustavoValdes/Prueba/controller/UserController.java
@@ -0,0 +1,35 @@
+package gustavovaldes.prueba.controller;
+
+import gustavovaldes.prueba.model.User;
+import gustavovaldes.prueba.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        try {
+            User savedUser = userService.save(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAll());
+    }
+}
